2f07f347f8d2699cc3224c851fb5ff966dd8dfcf
Максим <maxim.nedavniy@gmail.com>
2024-02-15 21:49:42 +0200

    HW41 complete

--- HomeWorkDjango/notes_app/urls.py
+++ HomeWorkDjango/notes_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('notes', views.notes, name='notes'),
+    path('notes/create', views.create_new_note, name='create_new_note')
 ]
